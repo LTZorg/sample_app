9f29b1dfc2e22772502c84b15ffc66e6668eb378
LTZorg <ltzorg@gmail.com>
2013-12-01 20:51:55 +0400

    Make a basic User model1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,7 +34,7 @@ describe "when name is too long" do
 
 describe "when email format is invalid" do
  	it "should be invalid" do
- 		addresses = %w[user@foo,com user_at_foo.org]
+ 		addresses = %w[user@foo,com user_at_foo.org foo@bar..com]
  		addresses.each do |invalid_addresses|
  			@user.email = invalid_addresses
  			expect(@user).not_to be_valid
@@ -101,4 +101,15 @@ describe "return value of authenticate method" do
   it { should be_invalid }
  end
 end
+
+describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
 end
